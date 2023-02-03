6b87c62275e281e5b5cb2a17f6aa962ee2760279
Dong <rlaehddnr1009@naver.com>
2023-02-03 20:23:34 +0900

    ErrorResponse 및 GlobalExceptionHandler를 도입

---
+++ src/main/java/tripmeeting/com/tripmeeting/domain/type/ErrorResponse.java
@@ -0,0 +1,17 @@
+package tripmeeting.com.tripmeeting.domain.type;
+
+import lombok.*;
+import org.springframework.http.HttpStatusCode;
+
+import java.util.Date;
+
+@Getter @Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse{
+    @NonNull
+    private String message;
+    private Date timestamp = new Date();
+    private StackTraceElement[] stack;
+
+}
